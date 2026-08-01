3ffa2bbf289e26673f77306eb2f3384eb131f3ad
Aleena Mariya V.A <aleenaponnus2003@gmail.com>
2026-08-01 14:48:14 +0530

    Time: 0 ms (100%), Space: 53.8 MB (90.91%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+    let count = 0;
+
+    for (let i = 1; i <= num; i++) {
+        let n = i;
+        let sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n = Math.floor(n / 10);
+        }
+
+        if (sum % 2 === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
