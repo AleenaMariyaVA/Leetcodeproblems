72d5c318831baf268e56a261142cdcd47287c300
Aleena Mariya V.A <aleenaponnus2003@gmail.com>
2026-08-07 12:11:35 +0530

    Time: 0 ms (100%), Space: 54.7 MB (54.49%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    let sum = 1;
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            sum += i;
+
+            if (i !== n / i) {
+                sum += n / i;
+            }
+        }
+    }
+
+    return sum === n;
+};
